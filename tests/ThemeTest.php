bdf85f4073781406fcc6afaacbe2913554e24250
karlomikus <contact@karlomikus.com>
2015-10-21 12:57:52 +0000

    Added setup for theme test

--- tests/ThemeTest.php
+++ tests/ThemeTest.php
@@ -2,43 +2,41 @@
 
 class ThemeTest extends TestCase {
 
+    private $theme;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $theme = new Karlomikus\Theme\Theme($this->app);
+        $theme->setDefaultThemePath(__DIR__ . '/stubs');
+        
+        $this->theme = $theme;
+    }
+
     public function testGetActiveTheme()
     {
-        $theme = $this->getTheme();
-        $theme->setTheme('test-theme');
+        $this->theme->setTheme('test-theme');
 
-        $this->assertEquals('test-theme', $theme->get()->getNamespace());
+        $this->assertEquals('test-theme', $this->theme->get()->getNamespace());
     }
 
     public function testGetAllThemes()
     {
-        $theme = $this->getTheme();
-
-        $this->assertArrayHasKey('test-theme', $theme->getThemes());
-        $this->assertArrayHasKey('parent-theme', $theme->getThemes());
+        $this->assertArrayHasKey('test-theme', $this->theme->getThemes());
+        $this->assertArrayHasKey('parent-theme', $this->theme->getThemes());
     }
 
     public function testSetTheme()
     {
-        $theme = $this->getTheme();
         $this->setExpectedException('Karlomikus\Theme\Exceptions\ThemeNotFoundException');
-        $theme->setTheme('throw-exception');
+        $this->theme->setTheme('throw-exception');
     }
 
     public function testThemeExists()
     {
-        $theme = $this->getTheme();
-
-        $this->assertNotTrue($theme->themeExists('test-false'));
-        $this->assertTrue($theme->themeExists('test-theme'));
-    }
-
-    private function getTheme()
-    {
-        $theme = new Karlomikus\Theme\Theme($this->app);
-        $theme->setDefaultThemePath(__DIR__ . '/stubs');
-
-        return $theme;
+        $this->assertNotTrue($this->theme->themeExists('test-false'));
+        $this->assertTrue($this->theme->themeExists('test-theme'));
     }
 
 }
